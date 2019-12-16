859bd0de3583c7890b632125db2334655e544c70
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-12-16 15:12:41 -0500

    nav removed from jsx

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,6 @@ const Home = () => (
       <link rel="icon" href="/favicon.ico" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"/>
     </Head>
-
-    <Nav />
     <main className="container construction">
       <span>
         <Phone /> 2 50 39 57
