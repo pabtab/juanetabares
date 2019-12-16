1893de9ed0743a66f576d50bcb6310d21be759dc
Pablo Tabares Hernandez <pablother3@gmail.com>
2019-12-16 12:41:08 -0500

    Contact adjustments

---
+++ components/Phone/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FaPhone } from 'react-icons/fa'
+
+
+const Phone = () => {
+  return (
+    <FaPhone />
+  )
+}
+
+export default Phone
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,33 @@
 import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
+import Whatsapp from '../components/Whatsapp'
+import Phone from '../components/Phone'
 
 const Home = () => (
   <div className="app">
     <Head>
-      <title>Consultorio Odontologico Juan Tabares</title>
+      <title>Juan Eugenio Tabares - Consultorio Odontologico</title>
       <link rel="icon" href="/favicon.ico" />
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css"/>
     </Head>
 
     <Nav />
     <main className="container construction">
+      <span>
+        <Phone /> 2 50 39 57
+      </span>
       <section className="notification is-primary"> 
-        <h1>🦷 - Sitio en Construccion - 🛠</h1>
+        <h1>🦷 - Sitio en Construcción - 🛠</h1>
       </section> 
+      <p>Escríbenos a:</p>
+      <Whatsapp />
     </main>
 
 
 
     <style global jsx>{`
-    html,
+      html,
       body,
       body > div:first-child,
       div#__next,
@@ -28,17 +35,23 @@ const Home = () => (
       div#__next > div > div {
         height: 100%;
       }
-
+    `}
+    </style>
+    <style jsx>{`
       .app {
-        display: flex;   
+        display: flex;
       }
 
       .construction {
         align-items: center;
         justify-content: center;
         display: flex;
+        flex-direction: column;
       }
 
+      .notification {
+        margin-top: 20px;
+      }
       
     `}</style>
   </div>
